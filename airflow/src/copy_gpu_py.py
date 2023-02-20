dff2988cf5fe41b52793e4bcac84c15da9831201
leejunseok <wt358@naver.com>
2023-02-20 11:31:40 +0900

    tad-gan excluded

--- airflow/src/copy_gpu_py.py
+++ airflow/src/copy_gpu_py.py
@@ -1132,5 +1132,8 @@ if __name__ == "__main__":
     elif sys.argv[1] == 'infer_main':
         print("entering inference main product ")
         infer_main()
+    elif sys.argv[1] == 'infer_ocsvm':
+        print("entering inference main product ")
+        infer_ocsvm()
     print("hello main")
  
