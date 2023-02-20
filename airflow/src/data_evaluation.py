59405bde9fe8f57cda61b51fde62ee8beee1793a
leejunseok <wt358@naver.com>
2023-02-20 13:31:10 +0900

    import asceding in pymongo

--- airflow/src/data_evaluation.py
+++ airflow/src/data_evaluation.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 from joblib import dump, load
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING, DESCENDING
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
