223a2bb19db4a26c1ae51d4b39e095dcc6100f3f
leejunseok <wt358@naver.com>
2023-02-21 17:39:52 +0900

    test Anomaly score

--- airflow/src/inference.py
+++ airflow/src/inference.py
@@ -410,17 +410,14 @@ def infer_lstm():
     Xtest = X_test.reshape(X_test.shape[0], X_test.shape[2])
     scored['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
     scored['Loss_mae'] = np.mean(np.abs(X_pred-Xtest), axis = 1)
-    mae_list=scored['Loss_mae'].tolist()
-    mean=np.mean(mae_list,axis=0)
-    std=np.cov(mae_list.T)
+    mean=np.mean(scored['Loss_mae'],axis=0)
+    std=np.cov(scored['Loss_mae'])
     print(mean)
     print(std)
     
-    x=mae_list-mean
-    print(x)
-    print(np.matmul(x,std))
+    # print(np.matmul(x,std))
     
-    scored['Anomaly_Score']=np.matmul(np.matmul(x,std),x.T)
+    scored['Anomaly_Score']=np.matmul(np.matmul((scored['Loss_mae']-mean),std),(scored['Loss_mae']-mean).T)
     scored['Threshold'] = 0.1
     scored['Anomaly'] = scored['Loss_mae'] > scored['Threshold']
     print(scored.head())
