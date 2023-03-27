81be1ad2de6060788d9421c82274d5ab688632e3
leejunseok <wt358@naver.com>
2023-03-27 16:21:14 +0900

    airflow test by different task

--- airflow/airflow-DAGS/test-dag.py
+++ airflow/airflow-DAGS/test-dag.py
@@ -60,8 +60,8 @@ with DAG(
     # Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
     for i in molding_brand_name:
         brand_name=i.lower()
-        fact=eval(brand_name)
-        fact_list=f'{fact}_factory_name'
+        fact=f'{brand_name}_factory_name'
+        fact_list=eval(fact)
         for j in fact_list:
             task = PythonOperator(
                 task_id='sleep_for_' + i + '_' + j,
