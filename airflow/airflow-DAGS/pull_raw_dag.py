26f7ca83f068d76325c8198f4c9573e8093ab987
leejunseok <wt358@naver.com>
2023-04-04 17:37:11 +0900

    multi index in the raw db

--- airflow/airflow-DAGS/pull_raw_dag.py
+++ airflow/airflow-DAGS/pull_raw_dag.py
@@ -216,7 +216,7 @@ def pull_mssql_woojin(**kwargs):
         client = MongoClient(host_mongo)
         db_test = client['raw_data']
         collection_test1 = db_test[f'{factory}_mold_data']
-        collection_test1.create_index([("TimeStamp",ASCENDING),("Additional_Info_1",TEXT),("Filling_Time",ASCENDING)],unique=True)
+        collection_test1.create_index([("TimeStamp",ASCENDING),("Additional_Info_1",TEXT),("Filling_Time",ASCENDING),("Injection_Time",ASCENDING)],unique=True)
         try:
             result = collection_test1.insert_many(data,ordered=False)
         except Exception as e:
