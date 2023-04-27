36d4c6aab49ef7d73b9cf533b0d59d2d7cb7d076
leejunseok <wt358@naver.com>
2023-04-27 15:24:43 +0900

    retries 3

--- airflow/airflow-DAGS/inference_dag.py
+++ airflow/airflow-DAGS/inference_dag.py
@@ -468,7 +468,7 @@ with DAG(
             # depends_on_past=True,
             depends_on_past=False,
             owner="coops2",
-            retries=0,
+            retries=3,
             retry_delay=timedelta(minutes=1),
             trigger_rule='none_failed_min_one_success',
         )
@@ -481,7 +481,7 @@ with DAG(
                 python_callable=eval('which_path_'+i),
                 op_kwargs={'brand_name':i,'factory_name':j},
                 dag=dag,
-                retries=0,
+                retries=3,
             )
             
             # infer_tadgan = KubernetesPodOperator(
@@ -525,7 +525,7 @@ with DAG(
                 is_delete_operator_pod=True,
                 get_logs=True,
                 startup_timeout_seconds=600,
-                retries=0,
+                retries=3,
             )
             
             infer_svm = KubernetesPodOperator(
@@ -548,7 +548,7 @@ with DAG(
                 is_delete_operator_pod=True,
                 get_logs=True,
                 startup_timeout_seconds=600,
-                retries=0,
+                retries=3,
             )
             
             infer_vari= KubernetesPodOperator(
@@ -571,7 +571,7 @@ with DAG(
                 is_delete_operator_pod=True,
                 get_logs=True,
                 startup_timeout_seconds=600,
-                retries=0,
+                retries=3,
             )
             # 테스크 순서를 정합니다.
             # t1 실행 후 t2를 실행합니다.
