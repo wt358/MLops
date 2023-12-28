ee799a593aa4ffce9b1f5b7ec20999433218e936
leejunseok <wt358@naver.com>
2023-12-28 16:19:05 +0900

    edit duration

--- airflow/airflow-DAGS/pull_raw_dag.py
+++ airflow/airflow-DAGS/pull_raw_dag.py
@@ -210,7 +210,7 @@ def pull_mssql_woojin(**kwargs):
         #         #outcsv.writerow(r)
 
         print("query result length: " + str(len(list(sql_result_pd))))
-        ind=50000
+        ind=60000
         df=df[ind:ind+10000]
         print(df['TimeStamp'])
         data=df.to_dict('records')
