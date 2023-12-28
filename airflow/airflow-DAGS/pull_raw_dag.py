6f74ce6c4b21efc8faca6900ab9a2c9b35cccb68
leejunseok <wt358@naver.com>
2023-12-28 16:02:01 +0900

    edit duration

--- airflow/airflow-DAGS/pull_raw_dag.py
+++ airflow/airflow-DAGS/pull_raw_dag.py
@@ -212,7 +212,7 @@ def pull_mssql_woojin(**kwargs):
         print("query result length: " + str(len(list(sql_result_pd))))
         ind=0
         df=df[ind:ind+10000]
-        print(df)
+        print(df['TimeStamp'])
         data=df.to_dict('records')
         host_mongo = Variable.get("WOOJIN_MONGO_URL_SECRET")
         client = MongoClient(host_mongo)
