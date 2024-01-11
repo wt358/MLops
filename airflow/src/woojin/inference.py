fd762a4635702bc8cc8604d6920020e4dc722a2d
leejunseok <wt358@naver.com>
2024-01-11 11:27:52 +0900

    duration edited

--- airflow/src/woojin/inference.py
+++ airflow/src/woojin/inference.py
@@ -78,143 +78,147 @@ def infer_ocsvm():
     # consumer.close()
     factory=os.environ['FACT_NAME']
     host = os.environ['MONGO_URL_SECRET'] 
-    client = MongoClient(host)
-    print(factory)
-    db_test = client['etl_data']
-    collection_etl=db_test[f'etl_{factory}']
-    start=now-timedelta(days=4)
-    query={
-            'TimeStamp':{
-                '$gt':start,
-                '$lt':now
+    for i in range(0,16):
+        client = MongoClient(host)
+        print(factory)
+        db_test = client['etl_data']
+        collection_etl=db_test[f'etl_{factory}']
+        start=now-timedelta(days=4)
+        start=now-timedelta(days=i*10)
+        start1=now-timedelta(days=(i+1)*10)
+        query={
+                'TimeStamp':{
+                    '$gt':start1,
+                    '$lt':start
+                    }
                 }
-            }
-    try:
-        df = pd.DataFrame(list(collection_etl.find(query)))
-    except Exception as e:
-        print("mongo connection failed", e)
-    client.close()
-    print(df)
-    if df.empty:
-        print("empty queue")
-        return
-    # dataframe transform
-    df=df[df['idx']!='idx']
-    df['TimeStamp']=pd.to_datetime(df['TimeStamp'],utc=True)
-    now=now.astimezone()
-    print(now)
-    start_time=now-timedelta(minutes=30)
-    start_time=now-timedelta(days=4)
-    print(start_time)
-    df=df[df['TimeStamp']>=start_time]
-    print(df.shape)
-    print(df.columns)
-    print(df)
+        try:
+            df = pd.DataFrame(list(collection_etl.find(query)))
+        except Exception as e:
+            print("mongo connection failed", e)
+        client.close()
+        print(df)
+        if df.empty:
+            print("empty queue")
+            return
+        # dataframe transform
+        df=df[df['idx']!='idx']
+        df['TimeStamp']=pd.to_datetime(df['TimeStamp'],utc=True)
+        now=now.astimezone()
+        print(now)
+        start_time=now-timedelta(minutes=30)
+        start_time=now-timedelta(days=4)
+        start_time=now-timedelta(days=(i+1)*10)
+        print(start_time)
+        df=df[df['TimeStamp']>=start_time]
+        print(df.shape)
+        print(df.columns)
+        print(df)
 
-    df.drop(columns={'_id',
-        },inplace=True)
-    
-    print(df)
-    if df.empty:
-        print("empty")
-        return 1
-    
-    labled = pd.DataFrame(df, columns = ['Filling_Time','Plasticizing_Time','Cycle_Time','Cushion_Position'])
-
-
-    labled.columns = map(str.lower,labled.columns)
-    labled.rename(columns={'class':'label'},inplace=True)
-    print(labled.head())
-
-    target_columns = pd.DataFrame(labled, columns = ['cycle_time', 'cushion_position'])
-    target_columns.astype('float')
-     
-     
-    host = os.environ['MONGO_URL_SECRET'] 
-    client=MongoClient(host)
-    db_model = client['model_var']
-    fs = gridfs.GridFS(db_model)
-    collection_model=db_model[f'OCSVM_{factory}']
-    
-    model_name = 'OC_SVM'
-    model_fpath = f'{model_name}.joblib'
-    result = collection_model.find({"model_name": model_name}).sort([("inserted_time", -1)])
-    print(result)
-    # cnt=len(list(result.clone()))
-    # print(result[0])
-    # print(result[cnt-1])
-    try:
-        file_id = str(result[0]['file_id'])
-        model = LoadModel(mongo_id=file_id).clf
-    except Exception as e:
-        print("exception occured in oc_svm",e)
-        model = OneClassSVM(kernel = 'rbf', gamma = 0.001, nu = 0.04).fit(target_columns)
-    joblib.dump(model, model_fpath)
-    
-    print(model.get_params())
-    
-    y_pred = model.predict(target_columns)
-    print(y_pred)
+        df.drop(columns={'_id',
+            },inplace=True)
+        
+        print(df)
+        if df.empty:
+            print("empty")
+            return 1
+        
+        labled = pd.DataFrame(df, columns = ['Filling_Time','Plasticizing_Time','Cycle_Time','Cushion_Position'])
 
 
+        labled.columns = map(str.lower,labled.columns)
+        labled.rename(columns={'class':'label'},inplace=True)
+        print(labled.head())
 
-    # filter outlier index
-    outlier_index = np.where(y_pred == -1)
+        target_columns = pd.DataFrame(labled, columns = ['cycle_time', 'cushion_position'])
+        target_columns.astype('float')
+        
+        
+        host = os.environ['MONGO_URL_SECRET'] 
+        client=MongoClient(host)
+        db_model = client['model_var']
+        fs = gridfs.GridFS(db_model)
+        collection_model=db_model[f'OCSVM_{factory}']
+        
+        model_name = 'OC_SVM'
+        model_fpath = f'{model_name}.joblib'
+        result = collection_model.find({"model_name": model_name}).sort([("inserted_time", -1)])
+        print(result)
+        # cnt=len(list(result.clone()))
+        # print(result[0])
+        # print(result[cnt-1])
+        try:
+            file_id = str(result[0]['file_id'])
+            model = LoadModel(mongo_id=file_id).clf
+        except Exception as e:
+            print("exception occured in oc_svm",e)
+            model = OneClassSVM(kernel = 'rbf', gamma = 0.001, nu = 0.04).fit(target_columns)
+        joblib.dump(model, model_fpath)
+        
+        print(model.get_params())
+        
+        y_pred = model.predict(target_columns)
+        print(y_pred)
 
-    #filter outlier values
-    outlier_values = target_columns.iloc[outlier_index]
-    print(outlier_values)
-    
-    # 이상값은 -1으로 나타낸다.
-    score = model.fit(target_columns)
-    anomaly = model.predict(target_columns)
-    target_columns['anomaly']= anomaly
-    anomaly_data = target_columns.loc[target_columns['anomaly']==-1] 
-    print(target_columns['anomaly'].value_counts())
 
-    target_columns[target_columns['anomaly']==1] = 0
-    target_columns[target_columns['anomaly']==-1] = 1
-    target_columns['Anomaly'] = target_columns['anomaly'] > 0.5
-    y_test = target_columns['Anomaly']
-    
-    print(y_test.unique())
 
-    # df = pd.DataFrame(labled, columns = ['label'])
-    # print(df.label)
-    
-    # outliers = df['label']
-    # outliers = outliers.fillna(0)
-    # print(outliers.unique())
-    # print(outliers)
+        # filter outlier index
+        outlier_index = np.where(y_pred == -1)
 
-    print(y_test)
-    y_log=pd.DataFrame(index=df.index)
-    y_log['TimeStamp']=df['TimeStamp']
-    y_log['Anomaly']=y_test
-    print(y_log['TimeStamp'].nunique())
-    # outliers = outliers.to_numpy()
-    # y_test = y_test.to_numpy()
+        #filter outlier values
+        outlier_values = target_columns.iloc[outlier_index]
+        print(outlier_values)
+        
+        # 이상값은 -1으로 나타낸다.
+        score = model.fit(target_columns)
+        anomaly = model.predict(target_columns)
+        target_columns['anomaly']= anomaly
+        anomaly_data = target_columns.loc[target_columns['anomaly']==-1] 
+        print(target_columns['anomaly'].value_counts())
 
-    # get (mis)classification
-    # cf = confusion_matrix(outliers, y_test)
+        target_columns[target_columns['anomaly']==1] = 0
+        target_columns[target_columns['anomaly']==-1] = 1
+        target_columns['Anomaly'] = target_columns['anomaly'] > 0.5
+        y_test = target_columns['Anomaly']
+        
+        print(y_test.unique())
 
-    # # true/false positives/negatives
-    # print(cf)
-    # (tn, fp, fn, tp) = cf.flatten()
+        # df = pd.DataFrame(labled, columns = ['label'])
+        # print(df.label)
+        
+        # outliers = df['label']
+        # outliers = outliers.fillna(0)
+        # print(outliers.unique())
+        # print(outliers)
 
-    # print(f"""{cf}
-    # % of transactions labeled as fraud that were correct (precision): {tp}/({fp}+{tp}) = {tp/(fp+tp):.2%}
-    # % of fraudulent transactions were caught succesfully (recall):    {tp}/({fn}+{tp}) = {tp/(fn+tp):.2%}
-    # % of g-mean value : root of (specificity)*(recall) = ({tn}/({fp}+{tn})*{tp}/({fn}+{tp})) = {(tn/(fp+tn)*tp/(fn+tp))**0.5 :.2%}""")
-    db_test = client['result_log']
-    collection = db_test[f'log_{model_name}_{factory}']
-    collection.create_index([("TimeStamp",pymongo.ASCENDING)],unique=True)
-    data=y_log.to_dict('records')
-    try:
-        collection.insert_many(data,ordered=False)
-    except Exception as e:
-        print("mongo connection failed",e)
-    client.close()
+        print(y_test)
+        y_log=pd.DataFrame(index=df.index)
+        y_log['TimeStamp']=df['TimeStamp']
+        y_log['Anomaly']=y_test
+        print(y_log['TimeStamp'].nunique())
+        # outliers = outliers.to_numpy()
+        # y_test = y_test.to_numpy()
+
+        # get (mis)classification
+        # cf = confusion_matrix(outliers, y_test)
+
+        # # true/false positives/negatives
+        # print(cf)
+        # (tn, fp, fn, tp) = cf.flatten()
+
+        # print(f"""{cf}
+        # % of transactions labeled as fraud that were correct (precision): {tp}/({fp}+{tp}) = {tp/(fp+tp):.2%}
+        # % of fraudulent transactions were caught succesfully (recall):    {tp}/({fn}+{tp}) = {tp/(fn+tp):.2%}
+        # % of g-mean value : root of (specificity)*(recall) = ({tn}/({fp}+{tn})*{tp}/({fn}+{tp})) = {(tn/(fp+tn)*tp/(fn+tp))**0.5 :.2%}""")
+        db_test = client['result_log']
+        collection = db_test[f'log_{model_name}_{factory}']
+        collection.create_index([("TimeStamp",pymongo.ASCENDING)],unique=True)
+        data=y_log.to_dict('records')
+        try:
+            collection.insert_many(data,ordered=False)
+        except Exception as e:
+            print("mongo connection failed",e)
+        client.close()
     print("hello oc_svm inference")
 
 
